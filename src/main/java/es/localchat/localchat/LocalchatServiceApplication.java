4eab943e4c4b2407ca8d20f4e271e63fd2b86350
sparkle <soroka.igor.olegovych@gmail.com>
2024-11-30 11:02:53 +0200

    init

---
+++ src/main/java/es/localchat/localchat/LocalchatServiceApplication.java
@@ -0,0 +1,28 @@
+package es.localchat.localchat;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Lazy;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LocalchatServiceApplication {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalchatServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/localchat/localchat/LocalchatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.localchat.localchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalchatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
